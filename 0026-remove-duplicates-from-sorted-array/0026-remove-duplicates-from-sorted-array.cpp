76c9839f921e2f2a670f290b61ef4cb247e6a38f
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-08-11 13:14:56 +0530

    Time: 0 ms (100%), Space: 22.5 MB (81.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,8 +3,8 @@ public:
     int removeDuplicates(vector<int>& arr) {
         int n=arr.size();
         int officer=0;
-        int uni=1;
-        int cm=1;
+        int unique=1;
+        int cm=officer+1;
         while(cm<n){
             if(arr[cm]==arr[cm-1]){
                 cm++;
@@ -12,10 +12,10 @@ public:
             else{
                 arr[officer+1]=arr[cm];
                 officer++;
-                uni++;
                 cm++;
+                unique++;
             }
         }
-        return uni;
+        return unique;
     }
 };

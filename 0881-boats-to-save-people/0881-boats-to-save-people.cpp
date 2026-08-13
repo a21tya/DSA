f5794f25fac5b56717cf8d2b551914b9de05e90e
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-08-13 16:23:03 +0530

    Time: 22 ms (24.6%), Space: 45.9 MB (38.92%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& arr, int limit) {
+        sort(arr.begin(), arr.end());
+        int n=arr.size();
+        int i=0;
+        int j=n-1;
+        int count=0;
+        while(i<=j){
+            if(arr[i]+arr[j]<=limit){
+                count++;
+                i++;
+                j--;
+            }
+            else{
+                count++;
+                j--;
+            }
+        }
+        return count;
+    }
+};

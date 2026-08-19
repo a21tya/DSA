53481ec219ca38dbced2084f8aa4282df906ab31
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-08-20 00:26:58 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        int n=arr.size();
-        int i=0;
-        int j=0;
-        while(i<n){
-            if(arr[i]!=0){
-                swap(arr[i],arr[j]);
-                    i++;
-                    j++;
-            }
-            else{
-                i++;
-            }
+     int n=arr.size();
+     int i=0;
+     int j=0;
+     while(j<n){
+        if(arr[j]!=0){
+            swap(arr[i],arr[j]);
+            i++;
+            j++;
         }
+        else{
+            j++;
+        }
+    }   
     }
 };

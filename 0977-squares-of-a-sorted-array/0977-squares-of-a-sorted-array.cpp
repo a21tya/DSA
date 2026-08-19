5e646accfd6d4ee951eebb363e2070954fe20431
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-08-20 00:19:34 +0530

    Time: 0 ms (100%), Space: 30.1 MB (99.15%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -4,8 +4,8 @@ public:
         int n=arr.size();
         int i=0;
         int j=n-1;
-        vector<int> sq(n);
         int k=n-1;
+        vector<int> sq(n);
         while(i<=j){
             if(abs(arr[i])>abs(arr[j])){
                 sq[k]=arr[i]*arr[i];

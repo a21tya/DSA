866f3baac4bcd259e341977da6fa86aa7328f677
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-08-20 01:01:40 +0530

    Time: 3 ms (98.19%), Space: 14 MB (85.85%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& arr, int target) {
-        int n=arr.size();
         sort(arr.begin(), arr.end());
-        int mdiff=INT_MAX;
+        int n=arr.size();
+        int i=0;
+        int min_diff=INT_MAX;
         int result_sum;
         for(int i=0; i<n; i++){
             int j=i+1;
             int k=n-1;
             while(j<k){
-                int sum=arr[i]+arr[j]+arr[k];
-                int diff=abs(sum-target);
+            int sum=arr[i]+arr[j]+arr[k];
+            int diff=abs(sum-target);
                 if(sum==target){
                     return sum;
                 }
-                if(diff<mdiff){
-                    mdiff=diff;
-                    result_sum=sum;
-                }
-                if(sum>target){
+                else if(sum>target){
                     k--;
-                    
                 }
-                else if(sum<target){
+                else{
                     j++;
+                }
+                if(diff<min_diff){
+                    min_diff=diff;
+                    result_sum=sum;
                 }
             }
         }
-        return result_sum;
+            return result_sum;
     }
 };

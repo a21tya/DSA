9e937255f1c27101c72efca4ece2fa3d44eb7418
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-08-21 23:10:04 +0530

    Time: 7 ms (73.23%), Space: 18.3 MB (39.82%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int len=0;
+        int left=0, right=0;
+        vector<int> hash(128,-1);
+        while(right< s.size()){
+            int preindex= hash[s[right]];
+            if(preindex!=-1 && preindex>=left ) {
+                left = preindex+1;
+            }
+            len = max(len,right-left+1);
+            hash[s[right]]=right;
+            right++;
+            //.
+
+        }
+        return len;
+    }
+}; 

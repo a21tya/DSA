084bfd2500f14536599b35fd4ae2c225baba9305
Aditya Yadav <aditya.24bcan0572@jecrcu.edu.in>
2026-09-04 23:38:58 +0530

    Time: 2 ms (2.71%), Space: 11.7 MB (48.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
-     int n=arr.size();
-     int start=0;
-     int mid=0;
-     int end=n-1;
-     while(mid<=end){
-        if(arr[mid]==0){
-            swap(arr[mid],arr[start]);
-            mid++;
-            start++;
-        }
-        else if(arr[mid]==1){
-            mid++;
-        }
-        else{// arr[mid]==2
-            swap(arr[mid],arr[end]);
-            end--;
-        }
-     }   
+        int n=arr.size();
+        int start=0;
+        int mid=0;
+        int end=n-1;
+        while(mid<=end){
+            if(arr[mid]==0){
+                swap(arr[mid], arr[start]);
+                start++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr[mid],arr[end]);
+                end--;
+            }
+        }//hioiii
     }
 };
